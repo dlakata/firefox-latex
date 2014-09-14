2efc3c86cc9e7ff39c4c176f79ddc8db847726ad
Prakhar Bhandari <prakhar23.bhandari@gmail.com>
2014-09-14 00:44:32 -0400

    spooky scary skeletons

---
+++ content.js
@@ -0,0 +1,10 @@
+var pdfElement = document.createElement("div");
+var baseURL = window.location.href.slice(0,-4) + ".pdf";
+var renderedPDF = "http://104.131.63.54:49178/" + baseURL;
+pdfElement.innerHTML = '<embed src="' + renderedPDF + '" style="position:fixed; top:0; right:0" width="50%" height="100%">';
+document.body.appendChild(pdfElement);
+var latex = document.getElementsByTagName('pre')[0];
+
+// CSS styling for LaTeX pane
+latex.style.width = "50%";
+latex.style.float = "left";
